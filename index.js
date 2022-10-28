1186ecb853839daa096bb623866b43f0de3a977c
Daria <zda199913@gmail.com>
2022-10-28 23:21:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const text = "Беларускі балотнічак сарамлівы";
 
-const textAnimation = (textNode) => {
+const textAnimation = (textNode, text) => {
     let index = 0;
 
-const intervel = setInterval(() =>{
+const interval = setInterval(() =>{
     debugger;
 index = index + 1;
 h1.textContent = text.slice(0, index);
@@ -13,13 +13,21 @@ if(index >= text.length){
 console.log(index, text.length)}, [100]);
 }
 
-const textChangeClasses = (h1) = {
+const textChangeClasses = (node) => {
+    const interval = setInterval(() => { 
+        node.classList.toggle();
+        node.style["right"] = "10px";
+    }, [500]);
+
+
 
 };
 
 document.addEventListener("DOMContentLoaded", () => {
 const h1 = document.querySelector("h1");
 const h2 = document.querySelector("h2");
-textAnimation(h1);
-hextAnimation(h2);
+const heat = document.querySelector("heat");
+textAnimation(h1, h1.textContent);
+textAnimation(h2, h2.textContent);
+//textAnimation(heat, h2.textContent);
 });
