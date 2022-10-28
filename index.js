f3ac3b1166b7e37634411a629accaa7f28b8123f
Daria <zda199913@gmail.com>
2022-10-28 22:09:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const text = "Беларускі балотнічак сарамлівы";
+
+document.addEventListener("DOMContentLoaded", () => {
+const h1 = document.querySelector("h1");
+let index = 0;
+
+const intervel = setInterval(() =>{
+index = index + 1;
+h1.textContent = index;
+if(index >50){
+    clearInterval(intervel);
+};
+console.log(h1)}, [3000]);
+});
